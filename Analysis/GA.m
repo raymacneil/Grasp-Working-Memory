bd40f8cd6e91ac641172c650721d38ec2158580a
raymacneil <raymond.macneil@alumni.utoronto.ca>
2025-02-05 14:14:06 -0800

    cleaned up code, no change to functionnality

--- Analysis/GA.m
+++ Analysis/GA.m
@@ -11,18 +11,12 @@
 
 function [A] = GA(posData)
 
-[rows cols] = size(posData);
-A = zeros(rows, 1);
+[rows, cols] = size(posData); %#ok<ASGLU>
 
 %'data' must have 6 columns to proceed
 if(cols == 6)
     A = sqrt(diag((posData(:,1:3)-posData(:,4:6))*transpose(posData(:,1:3)-posData(:,4:6))));
-    %x2_x1_sq = sqvec(posData(:,4) - posData(:,1));
-    %y2_y1_sq = sqvec(posData(:,5) - posData(:,2));
-    %z2_z1_sq = sqvec(posData(:,6) - posData(:,3));
-    %A = sqrt(x2_x1_sq  + y2_y1_sq + z2_z1_sq);
-    
-%otherwise, return an empty matrix
+
 else
     disp('Error! GA: data must have 6 columns. Returned an empty matrix!!!')
     A = [];
