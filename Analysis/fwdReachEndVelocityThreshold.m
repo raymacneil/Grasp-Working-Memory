5cd02a78f082056e20ce39422f2bbb65179f9974
raymacneil <raymond.macneil@alumni.utoronto.ca>
2025-02-18 13:36:05 -0800

    updated VDoff threshold, and fixed 'add one frame because it's velocity', now irrelevant

--- Analysis/fwdReachEndVelocityThreshold.m
+++ Analysis/fwdReachEndVelocityThreshold.m
@@ -1,35 +1,37 @@
-function fFwdOff = fwdReachEndVelocityThreshold(MkrVelocityData, PGAFrame, VToff, VDoff)
+function [fFwdOff] = fwdReachEndVelocityThreshold(MkrVelocityData, PGAFrame, VToff, VDoff)
 
 
-    VToffDefault = 75; % Default Velocity Threshold
-    VDoffDefault = 10; % Default Duration Velocity Threshold Met
-    
-    % Define default theshold parameters if not provided
-    if nargin < 3
-        VToff = VToffDefault;  
-        VDoff = VDoffDefault;   
-    elseif nargin < 4
-        VDoff = VDoffDefault;  
-    end
+VToffDefault = 75; % Default Velocity Threshold
+VDoffDefault = 5; % Default Duration Velocity Threshold Met
+
+% Define default theshold parameters if not provided
+if nargin < 3
+    VToff = VToffDefault;
+    VDoff = VDoffDefault;
+elseif nargin < 4
+    VDoff = VDoffDefault;
+end
 
 
-    VToffMet = find(MkrVelocityData < VToff);
-    Windows = cumsum([true; diff(VToffMet) ~= 1]);
-    VToffMetWindows = splitapply(@(x) {x}, VToffMet, Windows);
-    
-    if isempty(VToffMet)
-        VDoffFirstWin = VToffMet;
-    else
-        ValidSearch = (cellfun(@min, VToffMetWindows) > PGAFrame) &...
-            (cellfun('size', VToffMetWindows, 1) >= VDoff);
-        VDoffFirstWin = find(ValidSearch,1,'first');
-    end
-        
-    if ~isempty(VDoffFirstWin)
-        fFwdOff = VToffMetWindows{VDoffFirstWin}(1) + 1; % Because it's velocity, add one frame
-    else
-        fFwdOff = NaN;
-    end
+VToffMet = find(MkrVelocityData < VToff);
+Windows = cumsum([true; diff(VToffMet) ~= 1]);
+VToffMetWindows = splitapply(@(x) {x}, VToffMet, Windows);
+
+if isempty(VToffMet)
+    VDoffFirstWin = VToffMet;
+else
+    ValidSearch = (cellfun(@min, VToffMetWindows) > PGAFrame) &...
+        (cellfun('size', VToffMetWindows, 1) >= VDoff);
+    VDoffFirstWin = find(ValidSearch,1,'first');
+end
+
+if ~isempty(VDoffFirstWin)
+    fFwdOff = VToffMetWindows{VDoffFirstWin}(1); % Because it's velocity, add one frame
+else
+    fFwdOff = NaN;
+end
+
+   
        
     
 end
